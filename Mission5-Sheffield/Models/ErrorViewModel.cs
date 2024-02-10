341b434cd7e53f862d5d290cc5d47821521190ac
Tyler Sheffield <tylersheffield427@gmail.com>
2024-02-09 17:47:52 -0700

    Add project files.

---
+++ Mission5-Sheffield/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission5_Sheffield.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission5-Sheffield/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// Tyler Sheffield Mission 5
+// Function to get values from the form and calculate the total
+$(document).ready(function () {
+    $('#calculate').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var hourlyRate = parseFloat($('#hourly-rate').text());
+        // Validate the input
+        if (isNaN(hours) || hours < 0) {
+            alert("Please enter a positive number for hours.");
+            return;
+        }
+        // output the total
+        var total = hours * hourlyRate;
+        $('#total').val(total.toFixed(2));
+    });
+});
